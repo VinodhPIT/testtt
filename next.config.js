35d980d604c90dd5dee02789208257ae06288079
VinodhPIT <vinodh.mn@pitsolutions.com>
2023-11-02 09:57:12 +0530

    fix/image load issue

--- next.config.js
+++ next.config.js
@@ -1,18 +1,11 @@
 /** @type {import('next').NextConfig} */
-
-
-
 const nextTranslate = require("next-translate-plugin");
-
-
 const nextConfig = {
   images: {
     domains: ["storage.googleapis.com"],
-    // unoptimized: true,
   },
   env: {
     apiDomain: 'https://apiadmin.inckd.com/web/api',
-   // apiDomain: "https://admin.inckd.com/web/api",
     googlePlacesApiKey: "AIzaSyDo8sjdevbkqLGUx_DFpFlYlQFb1FpRAIo",
   },
 
