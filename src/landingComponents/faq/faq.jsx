939a6102602e94b3aaac436fa62df266cd7afd45
VinodhPIT <vinodh.mn@pitsolutions.com>
2023-11-20 13:33:23 +0530

    added additional components

---
+++ src/landingComponents/faq/faq.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Image from "next/image";
+import { blurDataURL } from "@/constants/constants";
+
+export default function Faq() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- src/pages/landingpage1/index.jsx
+++ src/pages/landingpage1/index.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
-import Image from 'next/image'
-import {blurDataURL} from '@/constants/constants'
-
+import React from "react";
+import Image from "next/image";
+import { blurDataURL } from "@/constants/constants";
+import Banner from "@/landingComponents/banner/banner";
+import PartnerTattoos from "@/landingComponents/partnerTattoos/tattoos";
+import TrendingArtist from "@/landingComponents/trendingArtist/trendingArtist";
+import Faq from "@/landingComponents/faq/faq";
 
 
 export default function Landing1() {
   return (
     <div>
-      
+      <Banner />
+
+      <PartnerTattoos />
+
+      <TrendingArtist />
+
+
+<Faq/>
+
+
+
+
+
     </div>
-  )
+  );
 }
