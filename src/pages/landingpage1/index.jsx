ea75b73a20fdd766a11bec29c8e8bff844926aaa
VinodhPIT <vinodh.mn@pitsolutions.com>
2023-11-20 13:01:23 +0530

    added next image

--- src/pages/landingpage1/index.jsx
+++ src/pages/landingpage1/index.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import Image from 'next/image'
+import {blurDataURL} from '@/constants/constants'
+
+
 
 export default function Landing1() {
   return (
--- src/pages/landingpage2/index.jsx
+++ src/pages/landingpage2/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Image from 'next/image'
+import {blurDataURL} from '@/constants/constants'
 
 export default function Landing2() {
   return (
